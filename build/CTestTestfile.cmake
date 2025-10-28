447cc69e83464a85d2f0c53335f9b83ed1da59c7
mcst2014 <mcst2014@outlook.com>
2025-10-27 20:56:24 -0700

    cmake build files

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/mcst2/OneDrive/Documents/Desktop/cmpm123_test/Assignment 4 Connect 4/connect-4
+# Build directory: C:/Users/mcst2/OneDrive/Documents/Desktop/cmpm123_test/Assignment 4 Connect 4/connect-4/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
